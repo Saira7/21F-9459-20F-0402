c87420031d6d9dc75bfe70e4583cf60def622201
Saira45 <f219459@cfd.nu.edu.pk>
2023-10-06 10:45:49 +0500

    Created CalculatorMain.java  only

---
+++ CalculatorMain.java
@@ -0,0 +1,15 @@
+
+public class CalculatorMain {
+
+	public CalculatorMain() {
+		
+	}
+
+	public static void main(String[] args) {
+		Calculator c =new Calculator();
+		c.add(2, 5);
+		c.sub(3, 2);
+
+	}
+
+}
