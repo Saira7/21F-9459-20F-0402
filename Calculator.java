323b0bf7cb435a816afdef8fba5521f9c3ece23f
Saira45 <f219459@cfd.nu.edu.pk>
2023-10-06 10:35:43 +0500

    Created Calculator.java with add & sub only

---
+++ Calculator.java
@@ -0,0 +1,14 @@
+
+public class Calculator {
+
+	public Calculator() {
+		// TODO Auto-generated constructor stub
+	}
+	double add(double num,double num1) {
+		return num+num1;
+	}
+	double sub(double num,double num1) {
+		return num-num1;
+	}
+
+}
